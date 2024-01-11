e1909a71a8288c6eb56e42254db3f4801f16af5d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-01-11 12:28:01 +0100

    Wygenerowanie modelu

--- src/DatabaseFirst/Program.cs
+++ src/DatabaseFirst/Program.cs
@@ -1,1 +1,4 @@
 ﻿Console.WriteLine("Hello, Database First!");
+
+
+// dotnet ef dbcontext scaffold "Data Source=.\SQLEXPRESS;Initial Catalog=sakila;Integrated Security=True;Encrypt=False" Microsoft.EntityFrameworkCore.SqlServer -o .\Model --context-dir .\Infrastructure -n Sakila.Domain.Model --context-namespace Sakila.Infrastructure
