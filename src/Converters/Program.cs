07ad8fa79c6e0e77a99bda930978d4f048e47a79
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-01-11 07:59:10 +0100

    Dodanie przykładów

--- src/Converters/Program.cs
+++ src/Converters/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, Conventions!");
+﻿using Infrastructure;
+
+Console.WriteLine("Hello, Value Converters!");
+
+using var context = new SakilaContext();
+
+var customers = context.Customers.ToList();
+
+customers.Dump();
