48bb03d6487b6e35bdeaa6c017f644a8502ce1c8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-01-12 15:27:03 +0100

    Dodanie Shadow Property

---
+++ src/ShadowProperty/Infrastructure/SakilaContext.Custom.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public partial class SakilaContext : DbContext
+{
+    public override int SaveChanges()
+    {
+        var addedEntities = ChangeTracker.Entries().Where(e=>e.State == EntityState.Added);
+
+        foreach (var entity in addedEntities)
+        {
+            entity.Property("created_date").CurrentValue = DateTime.UtcNow;
+        }
+
+        return base.SaveChanges();
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Actor>()
+            .Property<DateTime>("created_date");
+            
+    }
+}
---
+++ src/ShadowProperty/Program.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, Shadow Property!");
+
+/*
+ALTER TABLE actor
+	ADD created_date datetime2
+*/
+
+var actor = new Actor { FirstName = "a", LastName = "b" };
+
+using var context = new SakilaContext();
+
+context.Actors.Add(actor);
+//context.Entry(actor).Property("created_date").CurrentValue = DateTime.UtcNow;
+
+context.SaveChanges();
+
+// Sortowanie z użyciem shadow property
+var actors = context.Actors
+	.OrderBy(a => EF.Property<DateTime>(a, "created_date"))
+	.ToList();
+
+actors.Dump();
