b0ef47d97553e9b95160ecfbb3cb03e51829e4e6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-01-12 13:39:27 +0100

    Dodanie obsługi natywnych transakcji

--- src/Transactions/Program.cs
+++ src/Transactions/Program.cs
@@ -1,15 +1,71 @@
 ﻿using Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 
 Console.WriteLine("Hello, Transactions!");
 
 using var context = new SakilaContext();
 
+// SavepointsTransaction(context);
+
+Console.ReadLine();
+
 // TODO: save in transaction
+// NativeTransaction(context);
 
-context.Customers.Add(new Customer { FirstName = "a", LastName = "a" });
-context.SaveChanges();
+static void SavepointsTransaction(SakilaContext context)
+{
+    using IDbContextTransaction transaction = context.Database.BeginTransaction();
 
-context.Customers.Add(new Customer { FirstName = "b", LastName = "b" });
-context.SaveChanges();
+    try
+    {
+        context.Actors.Add(new Actor { FirstName = "a", LastName = "a" });
+        context.SaveChanges();
+
+        transaction.CreateSavepoint("BeforeMoreActors");
+
+        context.Actors.Add(new Actor { FirstName = "b", LastName = "b" });
+        context.Actors.Add(new Actor { FirstName = "c", LastName = "c" });
+
+        context.SaveChanges();
+
+
+        throw new ApplicationException();
+
+        transaction.Commit();
+
+
+
+
+    }
+    catch (Exception)
+    {
+        transaction.RollbackToSavepoint("BeforeMoreActors");
+    }
+
+}
+
+static void NativeTransaction(SakilaContext context)
+{
+    IDbContextTransaction transaction = context.Database.BeginTransaction();
+
+    try
+    {
+        context.Actors.Add(new Actor { FirstName = "a", LastName = "a" });
+        context.SaveChanges();
+
+        context.Actors.Add(new Actor { FirstName = "b", LastName = "b" });
+        context.SaveChanges();
+
+        throw new ApplicationException();
+
+        // transaction.Commit();
+
+    }
+    catch (Exception)
+    {
+        // transaction.Rollback();
+    }
+    
+}
 
